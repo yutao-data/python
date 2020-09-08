5a9ceb12131249d36c194f1ba91e58b30c175e14
A-hungry-wolf <yutao.chen@aliyun.com>
2020-09-08 22:59:43 +0800

    practice

---
+++ study/bird.py
@@ -0,0 +1,20 @@
+class Bird:
+    def __init__(self):
+        self.hungry = True
+
+    def eat(self):
+        if self.hungry:
+            print('Aaaah')
+            self.hungry = False
+        else:
+            print('No, thanks!')
+
+
+class SongBird(Bird):
+    def __init__(self):
+        # Bird.__init__(self)
+        super().__init__()
+        self.sound = 'Squawk!'
+
+    def sing(self):
+        print(self.sound)
