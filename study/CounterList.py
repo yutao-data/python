beefe6c024accfc2d928a0170257f9d6b9afc88b
A-hungry-wolf <yutao.chen@aliyun.com>
2020-09-10 15:44:13 +0800

    八皇后，迭代器学习

---
+++ study/CounterList.py
@@ -0,0 +1,8 @@
+class CounterList(list):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.counter = 0
+
+    def __getitem__(self, index):
+        self.counter += 1
+        return super(CounterList, self).__getitem__(index)
